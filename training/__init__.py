c45d2b3b1811b4f5f57d5ece4820731890d8f2ef
zjw131f1fc <zjw5428c@gmail.com>
2026-02-23 00:42:16 +0800

    删除未使用的AttackTrainer

--- training/__init__.py
+++ training/__init__.py
@@ -1,9 +1,7 @@
 """
 Training 模块 - 训练器
 """
-
-from .attack_trainer import AttackTrainer
 from .defense_trainer import DefenseTrainer
 from .finetuner import AutoFineTuner
 
-__all__ = ['AttackTrainer', 'DefenseTrainer', 'AutoFineTuner']
+__all__ = ['DefenseTrainer', 'AutoFineTuner']
